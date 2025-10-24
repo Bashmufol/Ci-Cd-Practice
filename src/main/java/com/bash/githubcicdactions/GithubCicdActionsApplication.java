cfd7e4131b1134c3f368e3796336416f5d7c7e46
Muhammed Bashir <bashmufol@gmail.com>
2025-10-24 17:15:34 +0100

    first commit

---
+++ src/main/java/com/bash/githubcicdactions/GithubCicdActionsApplication.java
@@ -0,0 +1,19 @@
+package com.bash.githubcicdactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubCicdActionsApplication {
+
+    public String welcome() {
+        return "Welcome to Cicd-Actions";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubCicdActionsApplication.class, args);
+    }
+
+}
